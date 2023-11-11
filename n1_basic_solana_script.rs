e9f1017f183784000b12fa1a407c7ed71f97fdfb
Farasat Ali <farasataliazeemi1@gmail.com>
2023-11-11 18:12:50 +0500

    Added Basic Solana Script

---
+++ n1_basic_solana_script.rs
@@ -0,0 +1,40 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+// Declare and export program's entrypoint
+entrypoint!(process_instructions);
+
+// program entrypoint's implementation
+pub fn process_instructions(
+    program_id: &Pubkey, // public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    _instruction_data: &[u8] // Ignored, all hello world instructions are hellos
+) -> ProgramResult {
+    msg!("Hello World Rust program entrypoint");
+
+    // Iterating accounts is safer than indexing
+    let accounts_iter = &mut accounts.iter();
+
+    // Get the account to say hello to
+    let account = next_account_info(accounts_iter);
+
+    // The account must be owned by a program in order to modify its data
+    if account.owner != program_id {
+        msg!("Greeted account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    // Increment and store the number of times the account has been greeted
+    let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+    greeting_account.counter += 1;
+    greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+
+    msg!("Greeted {} time(s)!", greeting_account.counter);
+
+    Ok(());
+}
